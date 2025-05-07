899e59dffd14bdad4b9f2a5f98ac12206a5fbc59
Einar671 <einarkaalhus2809@gmail.com>
2025-05-07 08:47:29 -0500

    Realizacion de graficos con la libreira matplotlib

--- src_python/app.py
+++ src_python/app.py
@@ -1,31 +1,55 @@
-# import benchmarking as Ben
+# import benchmarking as Ben # This line is commented out and can be removed
 from benchmarking import Benchmarking
 from metodos_ordenamiento import MetodosOrdenamiento
+import matplotlib.pyplot as plt
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print("Funciona")
-    
-    #Instancias
+
+    # Instancias
     metodos = MetodosOrdenamiento()
     bench = Benchmarking()
-    
-    
-    tamaño = 10000
-    arreglo_base = bench.buildArreglo(tamaño)
-   
-    
+
+    tamaños = [500, 1000, 2000]
+    resultados = []
+
     metodosDick = {
-        "burbuja":metodos.sortByBubble,
-        "seleccion":metodos.sortBySelection
-        }
+        "burbuja": metodos.sortByBubble,
+        "seleccion": metodos.sortBySelection
+    }
 
-    resultados= []
+    for tam in tamaños:
+        arreglo_base = bench.buildArreglo(tam)
 
-    for nombre,metodo in metodosDick.items():
-        tiempo = bench.medir_tiempo(metodo, arreglo_base)
-        tuplaresultado= (tamaño, nombre,tiempo)   
-        resultados.append(tuplaresultado)
-    
-    for resultados in resultados:
-        tamaño,nombre,tiempo=resultados
+        for nombre, metodo in metodosDick.items():
+            arreglo_para_ordenar = arreglo_base[:]
+            tiempo = bench.medir_tiempo(metodo, arreglo_para_ordenar)
+            tuplaresultado = (tam, nombre, tiempo)
+            resultados.append(tuplaresultado)
+
+    for tamaño, nombre, tiempo in resultados:
         print(f"Tamaño: {tamaño}, Metodo: {nombre}, Tiempo: {tiempo:.6f} segundos ")
+
+    tiempos_by_metodo = {}
+    for nombre in metodosDick.keys():
+        tiempos_by_metodo[nombre] = []
+
+    for tam, nombre, tiempo in resultados:
+        tiempos_by_metodo[nombre].append(tiempo)
+
+    plt.figure(figsize=(10, 6))
+
+    
+
+    for nombre, tiempos in tiempos_by_metodo.items():
+        if nombre in tiempos_by_metodo:
+             plt.plot(tamaños, tiempos_by_metodo[nombre], label=nombre, marker='o')
+
+
+    plt.xlabel("Tamaño del Arreglo")
+    plt.ylabel("Tiempo de Ejecución (segundos)")
+    plt.title("Tiempo de Ejecución de Métodos de Ordenamiento vs. Tamaño del Arreglo")
+    plt.legend() 
+
+    plt.grid(True) 
+    plt.show()
---
+++ src_python/grafica.py
@@ -0,0 +1,17 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5]
+y=[2,4,6,8,10]
+
+nombre_linea= "Linea 1"
+img = mpimg.imread("C:/Usuarios/USUARIO/Descargas/OIP.jpg")
+plt.plot(x,y,label= nombre_linea, color = "red")
+
+
+plt.title("Mi primer grafico")
+plt.xlabel("Datos eje X")
+plt.ylabel("Datos eje Y")
+
+plt.legend()
+plt.show()
